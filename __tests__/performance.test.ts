1df90a69a99971d7ee3515a689e302ad3d3a7488
ahmetilhn <ahmetilhan.dev@gmail.com>
2024-08-27 03:52:17 +0300

    added performance tests and improved memofy

---
+++ __tests__/performance.test.ts
@@ -0,0 +1,51 @@
+import memofy from "../lib";
+describe("performance tests", () => {
+  test("should execute heavy method with and without caching", async () => {
+    const heavyComputation = jest.fn(
+      async (inputNumber: number): Promise<number> => {
+        let sum = 0;
+        function isPrime(num: number): boolean {
+          if (num <= 1) return false;
+          if (num <= 3) return true;
+          if (num % 2 === 0 || num % 3 === 0) return false;
+          let i = 5;
+          while (i * i <= num) {
+            if (num % i === 0 || num % (i + 2) === 0) return false;
+            i += 6;
+          }
+          return true;
+        }
+        for (let i = 2; i <= inputNumber; i++) {
+          while (inputNumber % i === 0) {
+            if (isPrime(i)) {
+              sum += i;
+            }
+            inputNumber /= i;
+          }
+        }
+        return sum;
+      }
+    );
+
+    // Before caching
+    const _heavyComputation = memofy(heavyComputation);
+    const startTime = performance.now();
+    const result = await _heavyComputation(91012323);
+    const endTime = performance.now();
+    const timeDiff = endTime - startTime;
+    console.log("before caching", timeDiff, " ms");
+    expect(result).toEqual(30337444);
+    expect(timeDiff).toBeGreaterThanOrEqual(45);
+    expect(heavyComputation).toHaveBeenCalledTimes(1);
+
+    // After caching
+    const newStartTime = performance.now();
+    const newResult = await _heavyComputation(91012323);
+    const newEndTime = performance.now();
+    const newTimeDiff = newEndTime - newStartTime;
+    expect(newResult).toEqual(30337444);
+    console.log("after caching", newTimeDiff + " ms");
+    expect(newTimeDiff).toBeLessThanOrEqual(1);
+    expect(heavyComputation).toHaveBeenCalledTimes(1); // Because value returned cache
+  });
+});
--- lib/index.ts
+++ lib/index.ts
@@ -6,18 +6,21 @@ export default function memofy<ReturnType>(
 ): (..._functionToMemoizeArgs: Array<any>) => ReturnType {
   if (typeof _functionToMemoize !== "function")
     throw new Error("functionToMemoize must be function");
+
   return (...args: Array<any>): ReturnType => {
     const cachedData: Map<any, any> | undefined =
       cacheStore.get(_functionToMemoize);
 
-    if (cachedData?.get(JSON.stringify(args))) {
-      return cachedData.get(JSON.stringify(args));
+    const stringifiedArgs = JSON.stringify(args);
+
+    if (cachedData?.get(stringifiedArgs)) {
+      return cachedData.get(stringifiedArgs);
     }
 
     const returnVal = _functionToMemoize(...args);
 
     const cacheContent: Map<any, any> = new Map();
-    cacheContent.set(JSON.stringify(args), returnVal);
+    cacheContent.set(stringifiedArgs, returnVal);
     cacheStore.set(_functionToMemoize, cacheContent);
 
     return returnVal;
