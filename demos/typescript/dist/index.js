4c9b04efac1a86390bc4cb373c3deddb8030d850
ahmetilhn <ahmetilhan.dev@gmail.com>
2024-08-29 02:48:48 +0300

    improved demo projects

--- demos/typescript/dist/index.js
+++ demos/typescript/dist/index.js
@@ -1,46 +1,35 @@
 import memofy from "memofy";
-
-const product = { name: "Test Product", price: 36 };
-
-const getProductPrice = (_product: typeof product): string => {
-  return _product.price.toLocaleString();
+var product = { name: "Test Product", price: 36 };
+var getProductPrice = function (_product) {
+    return _product.price.toLocaleString();
 };
-
-const _getPrice = memofy(getProductPrice, []);
+var _getPrice = memofy(getProductPrice, []);
 // Runs once because uncached
 console.log(performance.now());
 console.log(_getPrice(product)); // 36
 console.log(performance.now());
-
 // Don't run because cached then fetch from cache
 console.log(performance.now());
 console.log(_getPrice(product)); // 36
 console.log(performance.now());
-
 product.price = 50;
 // Runs again because dep changed (production)
 console.log(performance.now());
 console.log(_getPrice(product)); // 50
 console.log(performance.now());
-
-const getProductName = (): string => {
-  return product.name;
+var getProductName = function () {
+    return product.name;
 };
-
-const _getProductName = memofy(getProductName, [product]);
-
+var _getProductName = memofy(getProductName, [product]);
 // Runs once because uncached
 console.log(performance.now());
 console.log(_getProductName()); // Test Product
 console.log(performance.now());
-
 // Don't run because cached
 console.log(performance.now());
 console.log(_getProductName()); // Test Product
 console.log(performance.now());
-
 product.name = "Test Product 2";
-
 // Runs again because dep changed (production)
 console.log(performance.now());
 console.log(_getProductName()); // Test Product
